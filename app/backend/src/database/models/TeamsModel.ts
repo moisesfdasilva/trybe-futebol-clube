ed92a65b1801a0d9d64c04e277eaa661ce4f0ebe
moisesfdasilva <msesfernandes@gmail.com>
2023-02-28 19:52:49 -0300

    feat: criada a model da tabela teams, em app/backend/TeamsModel.ts.

---
+++ app/backend/src/database/models/TeamsModel.ts
@@ -0,0 +1,27 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  declare readonly id: number;
+  declare teamName: string;
+}
+
+Teams.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  displayName: {
+    allowNull: false,
+    type: STRING,
+  }
+}, {
+  sequelize: db,
+  underscored: true,
+  timestamps: false,
+  modelName: 'teams',
+});
+
+export default Teams;
