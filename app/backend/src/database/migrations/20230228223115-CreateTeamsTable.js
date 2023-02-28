67f63a5e0533c00faabfe0b25eb44d3d2f239315
moisesfdasilva <msesfernandes@gmail.com>
2023-02-28 19:40:00 -0300

    feat: criada a migração da tabela teams.

---
+++ app/backend/src/database/migrations/20230228223115-CreateTeamsTable.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('teams', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      teamName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'team_name',
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('teams');
+  }
+};
