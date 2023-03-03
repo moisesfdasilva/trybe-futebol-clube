13da38b51ee8fadd2c7d96a0defa839311621ae3
moisesfdasilva <msesfernandes@gmail.com>
2023-03-03 09:40:38 -0300

    refactor: desestruturado o objeto recebido do body, em loginAuthMiddware.ts.

--- app/backend/src/middwares/loginAuthMiddware.ts
+++ app/backend/src/middwares/loginAuthMiddware.ts
@@ -2,17 +2,17 @@ import { Request, Response, NextFunction } from 'express';
 import validateEmail from '../utils/validateEmail';
 
 function loginAuthMiddware(req: Request, res: Response, next: NextFunction) {
-  const body = req.body;
-  if (!body.email) {
+  const { email, password } = req.body;
+  if (!email) {
     return res.status(400).json({ message: 'All fields must be filled' });
   }
-  if (!body.password) {
+  if (!password) {
     return res.status(400).json({ message: 'All fields must be filled' });
   }
-  if (validateEmail(body.email) === false) {
+  if (validateEmail(email) === false) {
     return res.status(401).json({ message: 'Invalid email or password' });
   }
-  if (body.password.length < 6) {
+  if (password.length < 6) {
     return res.status(401).json({ message: 'Invalid email or password' });
   }
   next();
